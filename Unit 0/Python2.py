cbacc8ca884de52f74072ec6f93fd0f278e4f174
Medha Pappula <medha.pappula@gmail.com>
2023-08-29 08:51:19 -0400

    94.25%

--- Unit 0/Python2.py
+++ Unit 0/Python2.py
@@ -11,7 +11,7 @@ def string_bits(str):
 
 def string_splosion(s):
   return ''.join(s[:i+1] for i in range(len(s)))
-
+  
 def last2(str):
   return sum(1 for i in range(len(str)-2) if str[i:i+2] == str[-2:])
 
@@ -33,16 +33,16 @@ def make_bricks(small, big, goal):
   return goal <= small + big * 5 and goal % 5 <= small
 
 def lone_sum(a, b, c):
-  return 0 if a == b == c else a if b == c else b if a == c else c if a == b else a + b + c
+  return sum(i for i in [a, b, c] if [a, b, c].count(i) == 1)
 
 def lucky_sum(a, b, c):
-  return 0 if a == 13 else a if b == 13 else a + b if c == 13 else a + b + c
+  return sum([a, b, c][:([a, b, c].index(13) if 13 in [a, b, c] else 3)])
 
 def no_teen_sum(a, b, c):
-  return sum(x if x not in [13, 14, 17, 18, 19] else 0 for x in [a, b, c])
+  return sum(x * (x not in {13, 14, 17, 18, 19}) for x in [a, b, c])
 
 def round_sum(a, b, c):
-  return sum((num // 10 + 1)*10 if num%10 >= 5 else (num // 10)*10 for num in [a, b, c])
+  return sum((n+5)//10*10 for n in [a, b, c])
 
 def close_far(a, b, c):
   return (abs(b - a) <= 1 and abs(c - a) >= 2 and abs(c - b) >= 2) or (abs(c - a) <= 1 and abs(b - a) >= 2 and abs(b - c) >= 2)
@@ -68,26 +68,26 @@ def end_other(a, b):
   return a.lower().endswith(b.lower()) or b.lower().endswith(a.lower())
 
 def xyz_there(str):
-  return True in ('xyz' in str[i:i+3] and (i == 0 or str[i-1] != '.') for i in range(len(str) - 2))
+  return 'xyz' in str.replace('.xyz', ' ')
 
 ## List - 2 ##
 
 def count_evens(nums):
-  return sum(1 for num in nums if num % 2 == 0)
+  return sum(1 for k in nums if ~k%2)
 
 def big_diff(nums):
   return max(nums) - min(nums)
 
 def centered_average(nums):
-  return (sum(nums) - max(nums) - min(nums)) // (len(nums) - 2)
+  return sum(sorted(nums)[1:-1])//(len(nums)-2)
 
 def sum13(nums):
   return sum(nums[i] for i in range(len(nums)) if nums[i] != 13 and (i == 0 or nums[i - 1] != 13))
 
 def sum67(nums):
-  return sum([x for index, x in enumerate(nums) if (((nums[(index - nums[index::-1].index(6)):].index(7) + (index - nums[index::-1].index(6))) < index) if (6 in nums[:index+1]) else True)])
+  return sum([x for i, x in enumerate(nums) if (((nums[(i - nums[i::-1].index(6)):].index(7) + (i - nums[i::-1].index(6))) < i) if (6 in nums[:i+1]) else True)])
 
 def has22(nums):
-  return sum(1 for n in range(len(nums)-1) if nums[n] == 2 and nums[n] == nums[n+1]) > 0
+  return ',2,2,' in ","+','.join(map(str, nums))+","
 
 #Medha Pappula, 6, 2026
