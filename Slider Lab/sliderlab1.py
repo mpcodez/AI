d40722e3fe8eb9ad95e7c3ad55683f112b7b0854
Medha Pappula <medha.pappula@gmail.com>
2023-09-13 20:04:25 -0400

    100% on sliderlab1

--- Slider Lab/sliderlab1.py
+++ Slider Lab/sliderlab1.py
@@ -1,5 +1,5 @@
-import sys;args=sys.argv[1:]
-import math 
+import sys; args = sys.argv[1:]
+import math
 import time
 
 gStart = ""
@@ -8,25 +8,62 @@ gWidth = 0
 gHeight = 0
 gSteps = []
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, item):
+        new_node = Node(item)
+        if self.tail is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        item = self.head.data
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        return item
+
+    def is_empty(self):
+        return self.head is None
+
 def puzzle(s, g):
     global gStart, gGoal, gWidth, gHeight
 
     gStart = s
-    l = [*s.replace("_", "")]
-    l.sort()
-    gGoal = "".join(l) + "_" if g == "" else g
+    if g == "":
+        l = [*s.replace("_", "")]
+        l.sort()
+        gGoal = "".join(l) + "_"
+    else:
+        gGoal = g
 
     gWidth = int(math.sqrt(len(s)))
     gHeight = int(len(s)/gWidth)
     while len(s)%gWidth != 0:
         gWidth += 1
         gHeight = int(len(s)/gWidth)
-    
+   
     if gHeight > gWidth:
         tmp = gWidth
         gWidth = gHeight
         gHeight = tmp
-    
+   
 def neighbors(p):
     i = p.index("_")
     val1 = switch(p, (i-gWidth), i) if 0 <= i-gWidth else ""
@@ -36,7 +73,7 @@ def neighbors(p):
     ret = set([val3, val4, val1, val2])
     ret.discard("")
     return list(ret)
-    
+   
 def switch(p, smol, big):
     return p[:smol] + p[big] + p[smol+1:big] + p[smol] + p[big+1:]
 
@@ -44,7 +81,7 @@ def output(p):
     for i in range(0, len(p), 5):
         band(p[i:i+5 if i+5 < len(p) else len(p)])
         print("")
-    
+   
 def band(p):
     for i in range(gHeight):
         val = ""
@@ -56,7 +93,7 @@ def count_inversions(puzzle_str):
 
     inversions = 0
     n = len(puzzle_str)
-    
+   
     for i in range(n):
         if puzzle_str[i] == '_':
             continue
@@ -65,29 +102,33 @@ def count_inversions(puzzle_str):
                 continue
             if puzzle_str[i] > puzzle_str[j]:
                 inversions += 1
-                
+               
     return inversions
 
-def is_solvable(start_state_str, goal_state_str):
+def rowOfSpace(st):
+    return int(st.index("_")/gWidth)
 
+def is_solvable(start_state_str, goal_state_str):
+   
     start_inversions = count_inversions(start_state_str)
     goal_inversions = count_inversions(goal_state_str)
-    
-    return (start_inversions % 2) == (goal_inversions % 2) or abs(start_inversions-goal_inversions) == 1
+   
+    return (gWidth%2 == 1 and (start_inversions % 2) != (goal_inversions % 2)) or (gWidth%2 == 0 and ((start_inversions+rowOfSpace(gStart)) % 2) != ((goal_inversions+rowOfSpace(gGoal)) % 2))
 
 def BFS(s, g):
-    
-    if not is_solvable(s, g):
+   
+    if is_solvable(s, g):
         return []
-        
+       
     if s == g:
         return [s]
 
-    parseMe = [s]
+    parseMe = Queue()
+    parseMe.enqueue(s)
     dctSeen = {s:" "}
 
     while parseMe:
-        node = parseMe.pop(0)
+        node = parseMe.dequeue()
         for nbr in neighbors(node):
             if nbr not in dctSeen.keys():
                 dctSeen[nbr] = node
@@ -98,7 +139,7 @@ def BFS(s, g):
                         ret.insert(0, tmp)
                         tmp = dctSeen[tmp]
                     return ret
-                parseMe.append(nbr)
+                parseMe.enqueue(nbr)
 
     return []
 
